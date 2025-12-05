0f315f14940605e7f7d654dc305825180970b6df
adrixay <darkuzuyt@gmail.com>
2025-12-05 10:12:40 -0500

    Add files via upload

---
+++ practica 6/2/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {5, 1, 4, 2, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Arreglo original:\n");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Arreglo ordenado:\n");
+    printArray(arr, n);
+
+    return 0;
+}
